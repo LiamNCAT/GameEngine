272fbb0f37f21a8db5029a3bccebfe19a44bf30c
William Nick <wmnick@aggies.ncat.edu>
2020-11-13 03:32:13 -0500

    added audio stuff

---
+++ src/audio.cpp
@@ -0,0 +1,21 @@
+#include <audio.h>
+
+AudioManager::AudioManager(){
+	
+}
+
+void AudioManager::load(string filename){
+	
+}
+
+void AudioManager::play(){
+	
+}
+
+void AudioManager::stop(){
+	
+}
+
+AudioManager::~AudioManager(){
+	
+}
